62886e832a2ce7c11ab73db7ad5619cb8bac76f2
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-26 01:09:41 +0300

    initial project setup

---
+++ src/main/java/com/premisave/wallet/PremisaveWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premisave/wallet/PremisaveWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.premisave.wallet;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PremisaveWalletServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
