d273607751088beff6b8ee9a4868873428001c27
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-26 01:24:09 +0300

    initial project setup

---
+++ src/main/java/com/premisave/wallet/PremisaveWalletApplication.java
@@ -0,0 +1,20 @@
+package com.premisave.wallet;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableAsync
+public class PremisaveWalletApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(PremisaveWalletApplication.class, args);
+    }
+}
